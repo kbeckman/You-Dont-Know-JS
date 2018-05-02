a08b7a04c3227f62963c81cb11ca840371d2a5b0
kbeckman <kbeckman@redfournine.com>
2018-05-02 09:19:53 -0500

    global - adding all books to global build script

--- build_epubs.sh
+++ build_epubs.sh
@@ -2,9 +2,27 @@
 
 mkdir -p epubs
 
-cd scope\ \&\ closures
+echo "Compiling YDKJS - Up & Going..."
+cd up\ \&\ going
 ./build_epub.sh
 
+echo "Compiling YDKJS - Scope & Closures..."
+cd ../scope\ \&\ closures
+./build_epub.sh
+
+echo "Compiling YDKJS - this & Object Prototypes..."
 cd ../this\ \&\ object\ prototypes
+./build_epub.sh
+
+echo "Compiling YDKJS - Types & Grammar..."
+cd ../types\ \&\ grammar
+./build_epub.sh
+
+echo "Compiling YDKJS - Async & Performance..."
+cd ../async\ \&\ performance
+./build_epub.sh
+
+echo "Compiling YDKJS - ES6 & Beyond..."
+cd ../es6\ \&\ beyond
 ./build_epub.sh
 
