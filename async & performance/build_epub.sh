8f61dd37dabc8c3e622678bb586818adb76759e3
kbeckman <kbeckman@redfournine.com>
2018-05-02 07:54:31 -0500

    async... - adding build_epub.sh and metadata.yml

---
+++ async & performance/build_epub.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# YDKJS - Scope & Closures
+pandoc toc.md \
+  foreword.md \
+  ../preface.md \
+  ch1.md \
+  ch2.md \
+  ch3.md \
+  ch4.md \
+  ch5.md \
+  ch6.md \
+  apA.md \
+  apB.md \
+  apC.md \
+  metadata.yml \
+  --epub-chapter-level=1 \
+  --standalone \
+  --output="../epubs/YDKJS - Async & Performance.epub"
+
